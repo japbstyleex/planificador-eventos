7edeece4d79a5f539cacb72b2610d39dca5054f3
japbstyleex <perales.bayon.jose.antonio@iescamas.es>
2024-11-28 11:20:13 +0100

    Actualizacion

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
-session_start();
+session_start(); // Iniciar sesión
+session_unset(); // Eliminar todas las variables de sesión
 session_destroy(); // Destruir la sesión
-header("Location: index.html"); // Redirigir al inicio
-exit();
+header("Location: index.html"); // Redirigir a la página principal
+exit;
 ?>
